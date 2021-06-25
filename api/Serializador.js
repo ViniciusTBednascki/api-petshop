4bb6e92d535a39276f8715e45eb4ec7c143a04af
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-06-24 22:18:40 -0300

    adicionando a serialização dos dados para xml

--- api/Serializador.js
+++ api/Serializador.js
@@ -1,15 +1,33 @@
 const ValorNaoSuportado = require("./erros/ValorNaoSuportado")
+const jsontoxml = require('jsontoxml')
 
 class Serializador {
     json (dados) {
         return JSON.stringify(dados)
     }
 
+    xml(dados) {
+        let tag = this.tagSingular
+
+        if(Array.isArray(dados)) {
+            tag = this.tagPlural
+            dados = dados.map((item) => {
+                return {
+                    [this.tagSingular]: item
+                }
+            })
+        }
+        return jsontoxml({[tag]: dados})
+    }
+
     serializar(dados) {
+        dados = this.filtrar(dados)
         if(this.contentType === 'application/json') {
-            return this.json(
-                this.filtrar(dados)
-            )
+            return this.json(dados)
+        }
+
+        if(this.contentType === 'application/xml') {
+            return this.xml(dados)
         }
 
         throw new ValorNaoSuportado(this.contentType)
@@ -45,6 +63,8 @@ class SerializadorFornecedor extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'empresa', 'categoria'].concat(camposExtras || [])
+        this.tagSingular = 'fornecedor'
+        this.tagPlural = 'fornecedores'
     }
 }
 
@@ -53,7 +73,8 @@ class SerializadorErro extends Serializador {
         super()
         this.contentType = contentType
         this.camposPublicos = ['id', 'mensagem'].concat(camposExtras || [])
-        
+        this.tagSingular = 'erro'
+        this.tagPlural = 'erros'
     }
 }
 
@@ -61,5 +82,5 @@ module.exports = {
     Serializador: Serializador,
     SerializadorFornecedor: SerializadorFornecedor,
     SerializadorErro: SerializadorErro,
-    formatosAceitos: ['application/json']
+    formatosAceitos: ['application/json', 'application/xml']
 }
