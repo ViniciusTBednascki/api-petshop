b0d5b46bad50af5c55f881a172e1839640365769
Vinicius T. Bednascki <viniciust.bednascki@gmail.com>
2021-07-02 21:17:29 -0300

    criando o método POST para a rota de produtos

--- api/rotas/fornecedores/produtos/tabelaProduto.js
+++ api/rotas/fornecedores/produtos/tabelaProduto.js
@@ -1,11 +1,14 @@
 const Modelo = require('./modeloTabelaProduto')
 
 module.exports = {
-listar(idFornecedor) {
+    listar(idFornecedor) {
         return Modelo.findAll({
             where: {
                 fornecedor: idFornecedor
             }
         })
+    },
+    inserir(dados) {
+        return Modelo.create(dados)
     }
 }
